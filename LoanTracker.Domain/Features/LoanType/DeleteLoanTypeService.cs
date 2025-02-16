596f2283a47f240b9a718d8dad917ca8bda10191
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2025-02-17 00:01:20 +0630

    feat: delete loan type

---
+++ LoanTracker.Domain/Features/LoanType/DeleteLoanTypeService.cs
@@ -0,0 +1,22 @@
+﻿public class DeleteLoanTypeService
+{
+    private readonly AppDbContext _context;
+
+    public DeleteLoanTypeService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Result<bool>> HandleAsync(DeleteLoanTypeRequest request)
+    {
+        var loanType = await _context.TblLoanTypes.FindAsync(request.LoanTypeId);
+        if (loanType == null)
+        {
+            return Result<bool>.NotFoundError("Loan Type not found.");
+        }
+
+        _context.TblLoanTypes.Remove(loanType);
+        await _context.SaveChangesAsync();
+        return Result<bool>.Success(true, "Loan Type deleted successfully.");
+    }
+}
