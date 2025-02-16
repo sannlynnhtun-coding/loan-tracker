64047246d43a24c5b4eec316763ff471c75b329f
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2025-02-17 00:01:09 +0630

    feat: create loan type

---
+++ LoanTracker.Domain/Features/LoanType/CreateLoanTypeRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace LoanTracker.Domain.Features.LoanType;
+
+public class CreateLoanTypeRequest
+{
+    public string LoanTypeName { get; set; } = null!;
+    public string? Description { get; set; }
+}
---
+++ LoanTracker.Domain/Features/LoanType/CreateLoanTypeService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoanTracker.Domain.Features.LoanType;
+
+namespace LoanTracker.Domain.Features.LoanType
+{
+}
+
+public class CreateLoanTypeService
+{
+    private readonly AppDbContext _context;
+
+    public CreateLoanTypeService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Result<LoanTypeResponse>> HandleAsync(CreateLoanTypeRequest request)
+    {
+        if (string.IsNullOrWhiteSpace(request.LoanTypeName))
+        {
+            return Result<LoanTypeResponse>.ValidationError("Loan Type Name is required.");
+        }
+
+        bool exists = await _context.TblLoanTypes.AnyAsync(l => l.LoanTypeName == request.LoanTypeName);
+        if (exists)
+        {
+            return Result<LoanTypeResponse>.ValidationError("Loan Type already exists.");
+        }
+
+        var loanType = new TblLoanType
+        {
+            LoanTypeName = request.LoanTypeName,
+            Description = request.Description,
+            CreatedDate = DateTime.UtcNow
+        };
+
+        _context.TblLoanTypes.Add(loanType);
+        await _context.SaveChangesAsync();
+
+        var response = new LoanTypeResponse
+        {
+            LoanTypeId = loanType.LoanTypeId,
+            LoanTypeName = loanType.LoanTypeName,
+            Description = loanType.Description,
+            CreatedDate = loanType.CreatedDate
+        };
+
+        return Result<LoanTypeResponse>.Success(response, "Loan Type created successfully.");
+    }
+}
